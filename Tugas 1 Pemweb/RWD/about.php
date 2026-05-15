741627b27412758f7f72bd983dbc2fae4571af85
Lulu Mukaromah <mukaromahlulu2307@gmail.com>
2026-05-15 12:48:37 +0700

    Add files via upload

---
+++ Tugas 1 Pemweb/RWD/about.php
@@ -0,0 +1,42 @@
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Hobby
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Hobi saya adalah membaca Novel dan Jalan-Jalan.</strong> Saya sangat suka membaca novel, saya sudah berulang kali membaca novel yang berjudul Eccedentesiast, di novel ini menceritakan pemuda yang amat sangat ingin di peluk oleh orang tua nya. Dan saya suka jalan-jalan berkeliling menggunakan transum. 
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Favorite Menu
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Menu favorit saya adalah ayam bakar dan chicken buldak yang ada di FamiliMart</strong> Saya suka ayam bakar karna ada sambel yang enak dan ada lalapan nya. Sedangkan chicken buldak nya FamuliMart itu enak banget, rasanya pedas dan gurih, cocok banget buat yang suka makanan pedas. Saya sering beli chicken buldak ini buat ngemil atau makan siang, rasanya selalu memuaskan dan bikin ketagihan!
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Pengalaman Organisasi
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>Karang Taruna</strong> adalah organisasi sosial kemasyarakatan yang bergerak di bidang kesejahteraan sosial. Organisasi ini biasanya terdiri dari para pemuda yang memiliki tujuan untuk membantu masyarakat dalam berbagai aspek kehidupan, seperti lingkungan, dan kegiatan sosial lainnya. Karang Taruna sering kali menjadi wadah bagi para pemuda untuk berkontribusi dalam pembangunan masyarakat dan meningkatkan kualitas hidup di lingkungan sekitar mereka.
+        <br></br>
+        <strong>Teshpa: ROAD TO END SEASON</strong> Sebagai bagian dari keluarga besar Teknik Informatika, Road to End Season adalah puncak perjalanan kami musim ini. Kami berkomitmen untuk menutup periode dengan dedikasi penuh, membawa semangat kolaborasi dan inovasi mahasiswa informatika hingga garis finish.
+        <br></br>
+        <strong>DPM</strong> merupakan lembaga tinggi legislatif di tingkat STT Terpadu Nurul Fikri yang memegang kekuasaan dalam pembentukan peraturan mahasiswa. DPM STT-NF berfungsi sebagai wadah aspirasi bagi seluruh mahasiswa, menjalankan fungsi pengawasan terhadap lembaga eksekutif, serta bertanggung jawab dalam menciptakan tata kelola organisasi kampus yang transparan dan akuntabel demi kemajuan civitas akademika.
+      </div>
+    </div>
+  </div>
+</div>
---
+++ Tugas 1 Pemweb/RWD/home.php
@@ -0,0 +1,23 @@
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="img/image.png" class="img-fluid rounded-start" alt="Foto Profil Saya" style="height: 100%; object-fit: cover;">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Profil Saya</h5>
+        <p class="card-text">
+          Halo! Nama saya Lu'luul Mukaromah. Saya adalah seorang Mahasiswi yang sedang menempuh pendidikan di STT Terpadu Nurul Fikri. Program Studi Teknik Informatika
+          Saya memiliki ketertarikan yang besar dalam dunia pengembangan web dan teknologi informasi.
+        </p>
+        <p class="card-text">
+          Website ini adalah wujud pengerjaan tugas pertama saya dalam mata kuliah Pemrograman Web, yang mengintegrasikan efisiensi Bootstrap dengan desain Bootswatch. Selain sebagai bukti pencapaian teknis, halaman ini juga menjadi jendela bagi Anda untuk mengenal lebih dekat perjalanan studi saya sebagai mahasiswa Informatika.
+          Melalui website ini, Anda dapat melihat perjalanan studi dan informasi kontak saya.
+        </p>
+        <p class="card-text">
+          <small class="text-body-secondary">Terakhir diperbarui: Baru saja</small>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
